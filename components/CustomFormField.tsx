7def03e56343b495d355d948639d3faa574d92d7
ashish8796 <ashishsaini.ak@gmail.com>
2024-07-11 15:31:06 +0530

    appwrite config and register form is being developed

--- components/CustomFormField.tsx
+++ components/CustomFormField.tsx
@@ -1,6 +1,9 @@
 import { Control } from "react-hook-form";
 import Image from "next/image";
 import "react-phone-number-input/style.css";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 import PhoneInput from "react-phone-number-input";
 import { FormFieldType } from "./forms/OnboardingForm";
 import {
@@ -68,6 +71,26 @@ const RenderField = ({ field, props }: { field: any; props: CustomProps }) => {
             className="input-phone"
           />
         </FormControl>
+      );
+
+    case FormFieldType.DATE_PICKER:
+      return (
+        <div className="flex rounded-md border border-dark-400">
+          <Image
+            src="/assets/icons/calendar.svg"
+            height={24}
+            width={24}
+            alt="calender"
+            className="ml-2"
+          />
+
+          <FormControl>
+            <DatePicker
+              selected={field.value}
+              onChange={(date) => field.onChange(date)}
+            />
+          </FormControl>
+        </div>
       );
 
     default:
