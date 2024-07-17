19c495177ac052f76070e86ca9ccf22f4108238a
ashish8796 <ashishsaini.ak@gmail.com>
2024-07-17 12:55:05 +0530

    added one more log

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -25,6 +25,8 @@ export const createUser = async (user: CreateUserParams) => {
       undefined,
       user.name
     );
+
+    console.log("New user: ", newUser);
     return parseStringify(newUser);
   } catch (error: any) {
     if (error && error?.code === 409) {
